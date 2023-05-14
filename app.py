06cae12d5fa05f30ba8d719d421382365ea6ba5e
andi598610 <andi598610@gmail.com>
2023-05-14 22:09:00 +0300

    connected pandasAI

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from data_processing import process_dataframe, process_journal
 from utils import get_table_download_link, to_excel
 from io import BytesIO
-from llm_agent import set_openai_key, load_data, create_agent, get_agent_response
+from llm_agent import set_openai_key, init_agent, get_agent_response
 
 st.title('Accounting Fast Close')
 
@@ -20,58 +20,63 @@ def load_excel_data(uploaded_file):
     df = pd.read_excel(uploaded_file)
     return df
 
+uploaded_file1 = st.sidebar.file_uploader('Upload your trial balance Excel file', type=['xlsx'])
+uploaded_file2 = st.sidebar.file_uploader('Upload your journal entry Excel file', type=['xlsx'])
+
+if uploaded_file1 is not None:
+    df1 = load_excel_data(uploaded_file1)
+    # Process the DataFrame
+    df1 = process_dataframe(df1)
+    excel_data = to_excel(df1)
+
+    # AI Agent Section
+    if openai_api_key:
+        agent = init_agent(openai_api_key)
+
+if uploaded_file2 is not None:
+    df2 = load_excel_data(uploaded_file2)
+    df2 = process_journal(df2)
+
+    # Comparing transactions
+    df1 = df1.merge(df2, on='Account', suffixes=('_TB', '_JE'))
+
+    # Compute the differences
+    df1['Diff Dr.'] = df1['Current Transactions Debit'] - df1['Debit Amount']
+    df1['Diff Cr.'] = df1['Current Transactions Credit'] - df1['Credit Amount']
+
 # Uploaded Documents Section
 if view_option == 'Uploaded Documents':
-    uploaded_file1 = st.sidebar.file_uploader('Upload your trial balance Excel file', type=['xlsx'])
     if uploaded_file1 is not None:
-        df1 = load_excel_data(uploaded_file1)
-
-        # AI Agent Section
-        if openai_api_key:
-            csv_file_path = load_data(uploaded_file1)
-            agent = create_agent(csv_file_path)
-
-            # Create a chat box for user questions
-            user_input = st.text_input('Ask a question:')
-            if user_input:
-                response = get_agent_response(agent, user_input)
-                st.write(response)
-
-        # Process the DataFrame
-        df1 = process_dataframe(df1)
-
-        excel_data = to_excel(df1)
-        #st.sidebar.markdown(get_table_download_link(excel_data, 'processed_data.xlsx'), unsafe_allow_html=True)
-
-        uploaded_file2 = st.sidebar.file_uploader('Upload your journal entry Excel file', type=['xlsx'])
-        if uploaded_file2 is not None:
-            try:
-                df2 = load_excel_data(uploaded_file2)
-                df2 = process_journal(df2)
-
-                # Comparing transactions
-                df1 = df1.merge(df2, on='Account', suffixes=('_TB', '_JE'))
-
-                # Compute the differences
-                df1['Diff Dr.'] = df1['Current Transactions Debit'] - df1['Debit Amount']
-                df1['Diff Cr.'] = df1['Current Transactions Credit'] - df1['Credit Amount']
-
-            except ValueError as e:
-                st.error(f"Error processing journal entry: {e}")
-
-        # Write the dataframe to the app
         st.write(df1)
+        st.markdown(get_table_download_link(excel_data, 'processed_data.xlsx'), unsafe_allow_html=True)
 
+    if uploaded_file2 is not None:
         # Save the dataframes to an Excel file
-        if uploaded_file2 is not None:
-            excel_data_combined = BytesIO()
-            with pd.ExcelWriter(excel_data_combined, engine='xlsxwriter') as writer:
-                df1.to_excel(writer, sheet_name='Trial Balance', index=False)
-                df2.to_excel(writer, sheet_name='Journal Entry', index=False)
+        excel_data_combined = BytesIO()
+        with pd.ExcelWriter(excel_data_combined, engine='xlsxwriter') as writer:
+            df1.to_excel(writer, sheet_name='Trial Balance', index=False)
+            df2.to_excel(writer, sheet_name='Journal Entry', index=False)
 
-            st.markdown(get_table_download_link(excel_data_combined.getvalue(), filename='combined.xlsx'), unsafe_allow_html=True)
+        st.markdown(get_table_download_link(excel_data_combined.getvalue(), filename='combined.xlsx'), unsafe_allow_html=True)
 
 elif view_option == 'Findings':
     # Logic for findings should be implemented here
-    st.write('Findings will be displayed here once implemented.')
+    st.write(df1)
+
+    # AI Agent Section
+    if openai_api_key and uploaded_file1 is not None:
+        # Create a chat box for user questions
+        user_input = st.text_input('Ask a question:')
+        if user_input:
+            response = get_agent_response(agent, df1, user_input)  # pass df1 as an argument
+            st.write(response)
+
+    if uploaded_file2 is not None:
+        # Save the dataframes to an Excel file
+        excel_data_combined = BytesIO()
+        with pd.ExcelWriter(excel_data_combined, engine='xlsxwriter') as writer:
+            df1.to_excel(writer, sheet_name='Trial Balance', index=False)
+            df2.to_excel(writer, sheet_name='Journal Entry', index=False)
+
+    st.markdown(get_table_download_link(excel_data_combined.getvalue(), filename='combined.xlsx'), unsafe_allow_html=True)
 
--- llm_agent.py
+++ llm_agent.py
@@ -1,34 +1,18 @@
 import os
 import pandas as pd
-from langchain.agents import create_csv_agent
-from langchain.llms import OpenAI
-import streamlit as st
-from data_processing import process_dataframe
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
 
 def set_openai_key(api_key):
     os.environ["OPENAI_API_KEY"] = api_key
+    return api_key
 
-@st.cache_data
-def convert_and_save_as_csv(uploaded_file):
-    df = pd.read_excel(uploaded_file)
-    df = process_dataframe(df)
-    csv_file_path = os.path.abspath('temp.csv')
-    df.to_csv(csv_file_path, index=False)
-    return csv_file_path
+def init_agent(api_key):
+    api_key = set_openai_key(api_key)
+    llm = OpenAI(api_token=api_key)
+    pandas_ai = PandasAI(llm, conversational=False)
+    return pandas_ai
 
-@st.cache_data
-def load_data(uploaded_file):
-    df = pd.read_excel(uploaded_file)
-    df.to_csv('temp.csv', index=False) 
-    return 'temp.csv'
-
-@st.cache_data
-def create_agent(q):
-    agent = create_csv_agent(OpenAI(temperature=0), './temp.csv', verbose=True)
-    return agent
-
-@st.cache_data
-def get_agent_response(agent, user_input):
-    context = """ The dataset is a trial balance."""
-    response = agent.run(user_input)
+def get_agent_response(agent, df, user_input):
+    response = agent.run(df, user_input)
     return response
